78b15518cd872b36f990f9751827cda1372ee81d
תהילה בן משה <bmtehila@gmail.com>
2023-05-29 14:15:45 +0300

    המשך תרגיל 6 שלב ב סיום סעיף 4

---
+++ src/lighting/PointLight.java
@@ -0,0 +1,90 @@
+package lighting;
+
+import primitives.Color;
+import primitives.Point;
+import primitives.Vector;
+
+public class PointLight extends Light implements LightSource {
+
+
+    private Point position; //The position point of the light source in the space
+
+    private double kC = 1, kL = 0, kQ = 0; // Light factor -> constant, linear and Quadratic initialize to 1, 0 ,0
+
+    /**
+     * Constructs a light with the given intensity.
+     *
+     * @param intensity the intensity of the light
+     */
+    protected PointLight(Color intensity, Point position) {
+        super(intensity);
+        this.position = position;
+    }
+
+    /**
+     * Calculate and return the intensity light on specific point
+     *
+     * @param point the point on the object (Point)
+     * @return the intensity (Color)
+     */
+    @Override
+    public Color getIntensity(Point point) {
+        // The intensity of the color of the light
+        // (the distribution of the light in the surface area)
+        // is proportional to squared distance
+
+        //Calculate the denominator of the proportion
+        double distance = this.position.distance(point);
+        double distanceSquared = distance * distance;
+        double factor = this.kC + this.kL * distance + this.kQ * distanceSquared;
+
+        //Return the final intensity
+        return getIntensity().reduce(factor);
+    }
+
+    /**
+     * Return normalize direction vector from the light source to the object
+     *
+     * @param point the point on the object (Point)
+     * @return normalize direction vector from the light source to the object (Vector)
+     */
+    @Override
+    public Vector getL(Point point) {
+        return point.subtract(this.position).normalize();
+    }
+
+    //******setter methods for the light attenuation coefficient:*****
+
+    /**
+     * setkC function
+     *
+     * @param kC
+     * @return
+     */
+    public PointLight setkC(double kC) {
+        this.kC = kC;
+        return this;
+    }
+
+    /**
+     * setkL function
+     *
+     * @param kL
+     * @return
+     */
+    public PointLight setkL(double kL) {
+        this.kL = kL;
+        return this;
+    }
+
+    /**
+     * setkQ function
+     *
+     * @param kQ
+     * @return
+     */
+    public PointLight setkQ(double kQ) {
+        this.kQ = kQ;
+        return this;
+    }
+}
